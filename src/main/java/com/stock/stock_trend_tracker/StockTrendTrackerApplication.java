ef1dee5c1b176ce10f129faaeb02447ce1834a6f
Ankit Singh <112419999+meCeltic@users.noreply.github.com>
2025-08-11 00:44:28 +0530

    spring boot baseline

---
+++ src/main/java/com/stock/stock_trend_tracker/StockTrendTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stock_trend_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrendTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrendTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/stock_trend_tracker/StockTrendTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.stock_trend_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrendTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
