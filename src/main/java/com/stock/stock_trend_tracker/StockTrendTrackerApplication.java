870a046c9f2e84af6d9fdc9fe899cf851967b298
Ankit Singh <112419999+meCeltic@users.noreply.github.com>
2025-08-11 00:47:13 +0530

    Add @EnableScheduling annotation to main Application class

--- src/main/java/com/stock/stock_trend_tracker/StockTrendTrackerApplication.java
+++ src/main/java/com/stock/stock_trend_tracker/StockTrendTrackerApplication.java
@@ -2,8 +2,10 @@ package com.stock.stock_trend_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockTrendTrackerApplication {
 
 	public static void main(String[] args) {
